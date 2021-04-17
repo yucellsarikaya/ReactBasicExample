7fcc8970c391f2d4ef3236e017a258e10d5fcbc7
yucellsarikaya <2017141050@cumhuriyet.edu.tr>
2021-04-18 00:02:03 +0300

    React basic props

---
+++ demo1/src/Add.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Add extends Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+            </div>
+        )
+    }
+}
---
+++ demo1/src/App.js
@@ -0,0 +1,11 @@
+import Add from "./Add";
+
+function App() {
+  return (
+    <div className="App">
+      <Add title={"Add adlı component cağırıldı ve bu cumle props olarak Add componentine gönderilidi!!!"}/>
+    </div>
+  );
+}
+
+export default App;
